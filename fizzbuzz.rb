7c0202d4b4f27096fd3f011cd8fd6f5e4dc56dbe
ZeroLive <zerolive@gmail.com>
2015-10-08 12:29:59 +0200

    Refactor with green tests

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,23 @@
 class FizzBuzz
 
   def self.answer number
-  	return "FizzBuzz" if number == 15
-  	return "Fizz" if number % 3 == 0
-  	return "Buzz" if number % 5 == 0
+  	return "FizzBuzz" if is_fizzbuzz?(number)
+	  return "Fizz" if is_fizz?(number)
+	  return "Buzz" if is_buzz?(number)
   	return number
   end
-  
-end
 
+  private
+
+  def self.is_fizzbuzz? number
+  	number == 15
+  end
+
+  def self.is_fizz? number
+  	number % 3 == 0
+  end
+
+  def self.is_buzz? number
+  	number % 5 == 0
+  end
+end
